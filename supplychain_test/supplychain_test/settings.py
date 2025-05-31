5fe09376e26c0b486c9b263f18a37c9654c4346e
KiishiAD <kiishi5695@gmail.com>
2025-05-31 17:10:56 +0100

    fixed login and logout page redirects changed logout to POST

--- supplychain_test/supplychain_test/settings.py
+++ supplychain_test/supplychain_test/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.urls import reverse_lazy
+
+LOGIN_REDIRECT_URL = reverse_lazy('supplychain:dashboard')
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
